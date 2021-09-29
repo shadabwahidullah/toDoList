66af15dc1ab5e35992e2db2eca9a4ff4c44454a0
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-30 00:11:12 +0430

    Design To do list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
-import _ from 'lodash';
-import './style.css';
+import _, { divide } from "lodash";
+import "./style.css";
 
-function component() {
-  const element = document.createElement('div');
+const toDoTasks = [
+  { desc: "Reading a book", completed: true, index: 1 },
+  { desc: "completing task", completed: true, index: 2 },
+  { desc: "Climbing mountain", completed: true, index: 3 },
+];
+const tasks = document.querySelector('.list');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+toDoTasks.forEach((e) => {
+  createTask(e);
+});
 
-  return element;
+addButton();
+
+function addButton() {
+  const btn = document.createElement('button');
+  btn.classList.add('button');
+  btn.innerHTML = 'Clear All Completed'
+  tasks.appendChild(btn);
 }
 
-document.body.appendChild(component());
+function createTask(e) {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('list-item', 'flex-space-between');
+
+  const element = document.createElement("div");
+  element.id = e.index;
+  element.classList.add('flex');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('margin');
+  checkBox.checked = e.completed;
+
+  const task = document.createElement('input');
+  task.type = 'text';
+  task.classList.add('text','margin');
+  task.value = e.desc;
+  task.readOnly = true;
+
+  const more = document.createElement('i');
+  more.classList.add('fa','fa-ellipsis-v','fa-v');
+
+  element.append(checkBox,task);
+  wrapper.append(element,more);
+
+  tasks.appendChild(wrapper);
+}
+
+
