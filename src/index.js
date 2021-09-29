13df390eea435c519c3603d1a06976ae21963e8f
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-30 00:22:32 +0430

    Resolve linters' errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
-import _, { divide } from "lodash";
-import "./style.css";
+import _, { divide } from 'lodash'; // eslint-disable-line no-unused-vars
+import './style.css';
 
 const toDoTasks = [
-  { desc: "Reading a book", completed: true, index: 1 },
-  { desc: "completing task", completed: true, index: 2 },
-  { desc: "Climbing mountain", completed: true, index: 3 },
+  { desc: 'Reading a book', completed: true, index: 1 },
+  { desc: 'completing task', completed: true, index: 2 },
+  { desc: 'Climbing mountain', completed: true, index: 3 },
 ];
+
 const tasks = document.querySelector('.list');
-
-toDoTasks.forEach((e) => {
-  createTask(e);
-});
-
-addButton();
-
-function addButton() {
-  const btn = document.createElement('button');
-  btn.classList.add('button');
-  btn.innerHTML = 'Clear All Completed'
-  tasks.appendChild(btn);
-}
 
 function createTask(e) {
   const wrapper = document.createElement('div');
   wrapper.classList.add('list-item', 'flex-space-between');
 
-  const element = document.createElement("div");
+  const element = document.createElement('div');
   element.id = e.index;
   element.classList.add('flex');
 
@@ -36,17 +24,28 @@ function createTask(e) {
 
   const task = document.createElement('input');
   task.type = 'text';
-  task.classList.add('text','margin');
+  task.classList.add('text', 'margin');
   task.value = e.desc;
   task.readOnly = true;
 
   const more = document.createElement('i');
-  more.classList.add('fa','fa-ellipsis-v','fa-v');
+  more.classList.add('fa', 'fa-ellipsis-v', 'fa-v');
 
-  element.append(checkBox,task);
-  wrapper.append(element,more);
+  element.append(checkBox, task);
+  wrapper.append(element, more);
 
   tasks.appendChild(wrapper);
 }
 
+toDoTasks.forEach((e) => {
+  createTask(e);
+});
 
+function addButton() {
+  const btn = document.createElement('button');
+  btn.classList.add('button');
+  btn.innerHTML = 'Clear All Completed';
+  tasks.appendChild(btn);
+}
+
+addButton();
