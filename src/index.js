e3c1703b475dc66a50018467f11500af66db2f1e
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-30 21:56:51 +0430

    Add trash icon for tasks

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
-import _, { divide } from 'lodash'; // eslint-disable-line no-unused-vars
-import * as statusModule from './status';
-import './style.css';
+import _, { divide } from "lodash"; // eslint-disable-line no-unused-vars
+import * as statusModule from "./status";
+import "./style.css";
 
-const tasks = document.querySelector('.list');
+const tasks = document.querySelector(".list");
 
 // creates a view for task t
 function createTask(t) {
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('list-item', 'flex-space-between');
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("list-item", "flex-space-between");
+  wrapper.id = t.index;
 
-  const element = document.createElement('div');
-  element.classList.add('flex');
+  const element = document.createElement("div");
+  element.classList.add("flex");
 
-  const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
-  checkBox.id = t.index;
-  checkBox.classList.add('margin');
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.classList.add("margin");
   checkBox.checked = t.completed;
 
-  checkBox.addEventListener('click', (event) => {
-    statusModule.statusUpdate(event.target.id, event.target.checked);
+  checkBox.addEventListener("click", (event) => {
+    statusModule.statusUpdate(event.target.parentElement.parentElement.id, event.target.checked);
   });
 
-  const task = document.createElement('input');
-  task.type = 'text';
-  task.classList.add('text', 'margin');
+  const task = document.createElement("input");
+  task.type = "text";
+  task.classList.add("text", "margin");
   task.value = t.desc;
   task.readOnly = true;
 
-  const more = document.createElement('i');
-  more.classList.add('fa', 'fa-ellipsis-v', 'fa-v');
+  const more = document.createElement("i");
+  more.classList.add("fa", "fa-ellipsis-v", "fa-v", "fa-2x", "margin");
+
+  const deleteBtn = document.createElement("i");
+  deleteBtn.classList.add("fa", "fa-trash", "fa-2x", "margin", "hidden");
+
+  // changes 3 dot icon to trash bin when mouse cursor gets over 3 dot
+  more.addEventListener("mouseover", () => {
+    more.classList.add("hidden");
+    deleteBtn.classList.remove("hidden");
+  });
+
+  // reverse the change for trash bin to 3 dots when the mouse cursor moves out
+  deleteBtn.addEventListener("mouseout", () => {
+    more.classList.remove("hidden");
+    deleteBtn.classList.add("hidden");
+  });
 
   element.append(checkBox, task);
-  wrapper.append(element, more);
+  wrapper.append(element, more, deleteBtn);
 
   tasks.appendChild(wrapper);
 }
@@ -41,11 +56,13 @@ statusModule.toDoTasks.forEach((e) => {
   createTask(e);
 });
 
-function addButton() {
-  const btn = document.createElement('button');
-  btn.classList.add('button');
-  btn.innerHTML = 'Clear All Completed';
+// create the a button at the end of list for
+// clearing the completed tasks
+function clearAllBtn() {
+  const btn = document.createElement("button");
+  btn.classList.add("button");
+  btn.innerHTML = "Clear All Completed";
   tasks.appendChild(btn);
 }
 
-addButton();
+clearAllBtn();
